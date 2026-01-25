1f30128b831b8cd2e74ed7f63fe0da49bb66c307
SOHYAMAZ <snow.sohei@gmail.com>
2026-01-25 19:38:02 +0900

    feat(phase04): I understood 'floodfill'

--- DFS/phase04.c
+++ DFS/phase04.c
@@ -6,7 +6,7 @@
 /*   By: sohyamaz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 20:05:07 by sohyamaz          #+#    #+#             */
-/*   Updated: 2026/01/25 18:10:06 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2026/01/25 19:31:01 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 // ============================================================
@@ -40,14 +40,14 @@ static void	dfs_phase04(char **input, int height, int width, int depth, int inde
 
 void	phase4_run(void)
 {
-	char	grid0[] = "#..##.#.";
-	char	grid1[] = "...##.#.";
-	char	grid2[] = "#.###.#.";
+	char	grid0[] = "##.##.#.";
+	char	grid1[] = ".#.##.#.";
+	char	grid2[] = "#####.#.";
 	char	grid3[] = "#.#####.";
 	char	grid4[] = "#..#....";
-	char	grid5[] = "##.##.#.";
+	char	grid5[] = ".#.##.#.";
 	char	grid6[] = "#..##.##";
-	char	grid7[] = "##.##.##";
+	char	grid7[] = "##.#####";
 	char	*input[] =
 	{
 		grid0,
@@ -67,7 +67,7 @@ void	phase4_run(void)
 		printf("input: %s\n", input[i]);
 		i++;
 	}
-	dfs_phase04(input, 8, 9, 0, 0);
+	dfs_phase04(input, 8, 8, 0, 0);
 	i = 0;
 	while(i < 8)
 	{
@@ -80,26 +80,62 @@ void	phase4_run(void)
 static void	dfs_phase04(char **input, int height, int width, int depth, int index)
 {
 	int	i;
+	int	up;
+	int	down;
+	int	left;
+	int	right;
 
-	if (*input == NULL || height == 0 || width == 0)
+	if (input == NULL || height == 0 || width == 0)
 	{
 		printf("Error: invalid arguments\n");
 		return ;
 	}
-	if (index >= width)
-	{
-		index = 0;
-		depth++;
-	}
-	if (height <= depth)
+	if (depth < 0 || depth >= height)
 		return ;
-	if (input[depth][index] == '#')
-		input[depth][index] = '.';
+	if (index < 0 || index >= width)
+		return ;
+	if (input[depth][index] != '#')
+		return ;
+	input[depth][index] = '.';
 	i = 0;
+	up = depth - 1;
+	down = depth + 1;
+	left = index - 1;
+	right = index + 1;
 	while(i < 8)
 	{
-		printf("current: %s\n", input[i]);
+		printf("draw: %s\n", input[i]);
 		i++;
 	}
-	dfs_phase04(input, height, width, depth, index + 1);
+	dfs_phase04(input, height, width, up, index);
+	dfs_phase04(input, height, width, down, index);
+	dfs_phase04(input, height, width, depth, left);
+	dfs_phase04(input, height, width, depth, right);
 }
+
+//static void	dfs_phase04(char **input, int height, int width, int depth, int index)
+//{
+//	int	i;
+//
+//	if (*input == NULL || height == 0 || width == 0)
+//	{
+//		printf("Error: invalid arguments\n");
+//		return ;
+//	}
+//	if (index >= width)
+//	{
+//		index = 0;
+//		depth++;
+//	}
+//	if (height <= depth)
+//		return ;
+//	if (input[depth][index] == '#')
+//		input[depth][index] = '.';
+//	i = 0;
+//	while(i < 8)
+//	{
+//		printf("draw: %s\n", input[i]);
+//		i++;
+//	}
+//	dfs_phase04(input, height, width, depth, index + 1);
+//}
