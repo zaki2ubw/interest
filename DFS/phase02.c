a872866ab6c65fb36e64a6a744b56cb80a34336d
SOHYAMAZ <snow.sohei@gmail.com>
2026-01-25 13:34:37 +0900

    feat(phase02): I understood vertical recurce func

--- DFS/phase02.c
+++ DFS/phase02.c
@@ -6,7 +6,7 @@
 /*   By: sohyamaz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 19:58:04 by sohyamaz          #+#    #+#             */
-/*   Updated: 2026/01/18 20:05:41 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2026/01/25 12:42:26 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 // ============================================================
@@ -41,9 +41,37 @@
 // ============================================================
 
 #include "dfs.h"
+#include <stdlib.h>
+#include <string.h>
+
+static void	dfs_phase2(char *input, size_t column, size_t depth);
 
 void	phase2_run(void)
 {
-	printf("[phase2] TODO\n");
+	char	input[] = "####.";
+	size_t		column;
+
+	printf("input: %s\n", input);
+	column = strlen(input);
+	dfs_phase2(input, column, 0);
+	printf("output: %s\n", input);
 	return ;
+}
+
+static void	dfs_phase2(char *input, size_t column, size_t depth)
+{
+	char	hashtag;
+	char	period;
+
+	hashtag = '#';
+	period = '.';
+	if (input == NULL || column == 0)
+		return ;
+	else if (input[depth] != period && input[depth] != hashtag)
+		return ;
+	if (input[depth] == period || column <= depth)
+		return ;
+	input[depth] = period;
+	printf("current: %s\n", input);
+	dfs_phase2(input, column, depth + 1);
 }
