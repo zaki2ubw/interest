d54e2386d272b21bba6827f800ef99813025798a
SOHYAMAZ <snow.sohei@gmail.com>
2026-01-25 16:32:31 +0900

    feat(phase03): I understood horizontal recursive func

--- DFS/phase03.c
+++ DFS/phase03.c
@@ -6,7 +6,7 @@
 /*   By: sohyamaz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 19:59:42 by sohyamaz          #+#    #+#             */
-/*   Updated: 2026/01/18 20:04:02 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2026/01/25 16:31:37 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 // ============================================================
@@ -33,9 +33,37 @@
 // ============================================================
 
 #include "dfs.h"
+#include <stdlib.h>
+#include <string.h>
+
+static void	dfs_phase3(char *input, size_t column, size_t depth);
 
 void	phase3_run(void)
 {
-	printf("[phase3] TODO\n");
+	char	input[] = "#####.....";
+	size_t		column;
+
+	printf("input: %s\n", input);
+	column = strlen(input);
+	dfs_phase3(input, column, 0);
+	printf("output: %s\n", input);
 	return ;
+}
+
+static void	dfs_phase3(char *input, size_t column, size_t depth)
+{
+	char	hashtag;
+	char	period;
+
+	hashtag = '#';
+	period = '.';
+	if (input == NULL || column == 0)
+		return ;
+	else if (input[depth] != period && input[depth] != hashtag)
+		return ;
+	if (column <= depth || input[depth] == period)
+		return ;
+	input[depth] = period;
+	printf("current: %s\n", input);
+	dfs_phase3(input, column, depth + 1);
 }
