cd2a492c5901a6f0595534ac671013f059b93e97
SOHYAMAZ <snow.sohei@gmail.com>
2026-01-31 17:56:08 +0900

    I cleared lake counting

--- DFS/dfs.h
+++ DFS/dfs.h
@@ -6,7 +6,7 @@
 /*   By: sohyamaz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 19:23:51 by sohyamaz          #+#    #+#             */
-/*   Updated: 2026/01/18 20:00:51 by sohyamaz         ###   ########.fr       */
+/*   Updated: 2026/01/31 17:34:16 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,15 @@
 # define DFS_H
 
 #include <stdio.h>
+
+typedef struct s_board	t_board;
+
+struct s_board
+{
+	char	**input;
+	int		length_x;
+	int		length_y;
+} ;
 
 void    phase0_run(int depth);
 void    phase1_run(void);
--- DFS/phase05.c
+++ DFS/phase05.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   phase05.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sohyamaz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sohyamaz <sohyamaz@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/18 20:07:49 by sohyamaz          #+#    #+#             */
-/*   Updated: 2026/01/18 20:08:52 by sohyamaz         ###   ########.fr       */
+/*   Created: 2026/01/31 15:56:56 by sohyamaz          #+#    #+#             */
+/*   Updated: 2026/01/31 17:55:30 by sohyamaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 // ============================================================
@@ -35,9 +35,121 @@
 // ============================================================
 
 #include "dfs.h"
+#include <stdlib.h>
+
+static int	count_lake(t_board *board, int length_x, int length_y);
+static void	floodfill(t_board *board, int origin_x, int origin_y);
 
 void	phase5_run(void)
 {
-	printf("[phase5] TODO\n");
+	//char	grid0[] = "WW.WW.W.";
+	//char	grid1[] = "...WW.W.";
+	//char	grid2[] = "W..W..WW";
+	//char	grid3[] = "W.......";
+	//char	grid4[] = "W.......";
+	//char	grid5[] = "...WW.W.";
+	//char	grid6[] = "W..WW.WW";
+	//char	grid7[] = "WW.WW..W";
+	char	grid0[] = "W.......";
+	char	grid1[] = ".W......";
+	char	grid2[] = "..W.....";
+	char	grid3[] = "...W....";
+	char	grid4[] = "....W...";
+	char	grid5[] = ".....W..";
+	char	grid6[] = "......W.";
+	char	grid7[] = ".......W";
+	char	*input[] =
+	{
+		grid0,
+		grid1,
+		grid2,
+		grid3,
+		grid4,
+		grid5,
+		grid6,
+		grid7
+	};
+	int	lake;
+	int	i;
+	t_board	*board;
+
+	i = 0;
+	while(i < 8)
+	{
+		printf("input: %s\n", input[i]);
+		i++;
+	}
+	board = (t_board *)calloc(1, sizeof(t_board));
+	board->input= input;
+	board->length_x = 8;
+	board->length_y = 8;
+	lake = count_lake(board, board->length_x, board->length_y);
+	i = 0;
+	while(i < 8)
+	{
+		printf("output: %s\n", input[i]);
+		i++;
+	}
+	//printf("Expect: 7\nResult: %d\n", lake);
+	//printf("Expect: 1\nResult: %d\n", lake);
+	printf("Expect: 8\nResult: %d\n", lake);
+	free(board);
 	return ;
+}
+
+static int	count_lake(t_board *board, int length_x, int length_y)
+{
+	int		y;
+	int		x;
+	int		lake;
+	char	water;
+
+	water = 'W';
+	y = 0;
+	lake = 0;
+	while (y < length_y)
+	{
+		x = 0;
+		while (x < length_x)
+		{
+			if (board->input[y][x] == water)
+			{
+				lake++;
+				floodfill(board, x, y);
+			}
+			x++;
+		}
+		y++;
+	}
+	return (lake);
+}
+
+static void	floodfill(t_board *board, int origin_x, int origin_y)
+{
+	//int	direct_x[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+	//int	direct_y[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+	int	direct_x[] = {-1, 0, 0, 1};
+	int	direct_y[] = {0, -1, 1, 0};
+	int	i;
+	int	target_x;
+	int	target_y;
+
+	if (board == NULL || board->input == NULL)
+		return ;
+	if (origin_x < 0 || origin_x >= board->length_x)
+		return ;
+	else if (origin_y < 0 || origin_y >= board->length_y)
+		return ;
+	if (board->input[origin_y][origin_x] != 'W')
+		return ;
+	board->input[origin_y][origin_x] = '.';
+	i = 0;
+	//while (i < 8)
+	while (i < 4)
+	{
+		target_x = origin_x + direct_x[i];
+		target_y = origin_y + direct_y[i];
+		floodfill(board, target_x, target_y);
+		i++;
+	}
 }
